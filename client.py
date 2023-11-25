90833307e02df62e5b552fd58c890ce0d5472ab2
AdrichJoseph <kippadrichjoseph@gmail.com>
2023-11-25 01:57:34 -0700

    implemented key generator/terminate (need encryption still), suggested some attacks and solutions. Added somethings to server/client.py

--- client.py
+++ client.py
@@ -12,6 +12,8 @@ import json
 import sys
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+#gang
+
 
 
 def client():
@@ -26,7 +28,44 @@ def client():
         clientSocket.connect((serverName, serverPort))
 
 
-#        while True:
+
+
+        # while loop for the client side
+
+        while True:
+
+            # ask for the client response
+            clientResponse = input()
+            clientSocket.send(clientResponse.encode('ascii'))
+
+            # if client is = 1 ,create and send email
+            if clientResponse == '1':
+                pass
+
+
+
+
+            # when user choose 2 print list of emails
+
+            elif clientResponse == '2':
+                pass
+
+            #if 3 ,  Display email contents
+            elif clientResponse == '3':
+                pass
+
+            # if 4, terminate  # still need to apply encryption
+            elif clientResponse == '4':
+                server_response = clientSocket.recv(2048).decode('ascii')
+                print(server_response)
+                clientSocket.close()
+                break
+
+            # client response is anything else,  send the message as 4 since the server will receive it and will automatically
+            # trigger the else statement in their side.
+
+            menu = clientSocket.recv(2048).decode('ascii')
+            print(menu, end='')
 
 
 
